f7e734251bcb6187905246dff65a2c568dcd9880
Talis M <talis_power@hotmail.com>
2022-12-01 16:27:23 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/talismonteiro/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.talismonteiro.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.talismonteiro.workshopmongo.domain.User;
+import com.talismonteiro.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
