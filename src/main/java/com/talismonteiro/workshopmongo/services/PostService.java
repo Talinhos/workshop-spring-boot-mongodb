beb7dc886c036947b2cb2529c5a24b33d1c2bdb4
Talis M <talis_power@hotmail.com>
2022-12-13 21:18:38 -0300

    Obtendo um post por id

---
+++ src/main/java/com/talismonteiro/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.talismonteiro.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.talismonteiro.workshopmongo.domain.Post;
+import com.talismonteiro.workshopmongo.domain.User;
+import com.talismonteiro.workshopmongo.repository.PostRepository;
+import com.talismonteiro.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
